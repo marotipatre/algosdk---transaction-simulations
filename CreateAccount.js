5ab8f1f647ea85bc1207d08d9e40bde843a5c130
maroti <vaibhavspatre@gmail.com>
2025-02-16 01:03:42 +0530

    account creation strcuture changed

--- CreateAccount.js
+++ CreateAccount.js
@@ -10,18 +10,24 @@ function keypress() {
 }
 
 async function createAccountAndExport() {
+  
   // Create an account
   const generatedAccount = algosdk.generateAccount();
   const passphrase = algosdk.secretKeyToMnemonic(generatedAccount.sk);
+
+  // Convert the private key to base64 string
+  const privateKeyBase64 = Buffer.from(generatedAccount.sk).toString('base64');
+
   console.log(`My address: ${generatedAccount.addr}`);
   console.log(`My passphrase: ${passphrase}`);
+  console.log(`My private key: ${privateKeyBase64}`);
+
+  c
+  // dispense testnet algos
   const dispenser_url = `https://bank.testnet.algorand.network/?account=${generatedAccount.addr}`;
   console.log(`Fund the wallet via Algorand Dispenser: ${dispenser_url}`);
   console.log("Press any key when the account is funded");
   await keypress();
-
-  // Convert the private key to base64 string
-  const privateKeyBase64 = Buffer.from(generatedAccount.sk).toString('base64');
 
   // Export the account details as JSON
   const accountData = {
